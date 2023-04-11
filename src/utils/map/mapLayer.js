085522a39b34aa396def1957d059928f70a9cde0
aisukdl <pk_aisu@hotmail.co.th>
2023-04-11 14:54:33 +0700

    add altitude and altitude line to map

--- src/utils/map/mapLayer.js
+++ src/utils/map/mapLayer.js
@@ -3,5 +3,14 @@ import { mapRoute } from './mapRoute'
 import { textMarkers,restroomMarkers } from './marker';
 
 
-const mapGeometries = [mapRoute,...textMarkers,...restroomMarkers]
-export const mapLayers = [new maptalks.VectorLayer('v', mapGeometries)]
+const layerGeometries = [mapRoute,...textMarkers,...restroomMarkers]
+const layerOptions = {
+    enableAltitude : true,
+    // draw altitude
+    drawAltitude : {
+      lineWidth : 1,
+      lineColor : '#000'
+    }
+}
+
+export const mapLayers = [new maptalks.VectorLayer('v', layerGeometries, layerOptions)]
